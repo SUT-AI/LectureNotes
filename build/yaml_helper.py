59560bf8675ddbbb1eaa0a10adf6b0d9c79e64df
ahmadsalimi <ahsa9978@gmail.com>
2021-04-22 20:09:21 +0430

    Faeture: Remove template files

--- build/yaml_helper.py
+++
@@ -1,8 +0,0 @@
-from typing import Union, Dict, List, Any
-import yaml
-
-YamlNode = Union[Dict[str, 'YamlNode'], List['YamlNode'], str]
-
-def read_yaml(path: str) -> YamlNode:
-    with open(path) as file:
-        return yaml.full_load(file)
